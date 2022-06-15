0cf8d126a7bde6722cf3b5d839e940f416f914ab
Sairam <sairam.vj@gmail.com>
2022-06-15 09:53:36 +0530

    Formatting

--- Cloud_Containers/gcp_terraform/main.tf
+++ Cloud_Containers/gcp_terraform/main.tf
@@ -1,9 +1,9 @@
 module "iam" {
-  source                =     "./modules/"
-  bucket                =     var.bucket 
-  project               =     var.project
-  dataset_id            =     var.dataset_id 
-  table_id              =     var.table_id
+  source                                        =     "./modules/"
+  bucket                                        =     var.bucket
+  project                                       =     var.project
+  dataset_id                                    =     var.dataset_id
+  table_id                                      =     var.table_id
   google_storage_bucket_iam_binding_role        =     var.google_storage_bucket_iam_binding_role 
   google_storage_bucket_iam_binding_users       =     var.google_storage_bucket_iam_binding_users
   google_bigquery_table_iam_binding_role        =     var.google_bigquery_table_iam_binding_role
